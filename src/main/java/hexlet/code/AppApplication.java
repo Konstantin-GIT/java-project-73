a0cef8b9c2cd512eb97455ded04cb8bb7c068ae0
Konstantin Naumovich <konstantin000008@gmail.com>
2023-11-13 11:57:17 +0300

    added @PropertySource

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import net.datafaker.Faker;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
